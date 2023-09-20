0a7382d0f65e541cd6501c3d3c2d7bb01c448319
smzaafir <128362703+smzaafir@users.noreply.github.com>
2023-09-20 15:15:53 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,279 @@
+<?php
+$insert = false;
+$update = false;
+$error = false;
+$delete = false;
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "notes";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+if (!$conn) {
+    die("Sorry, we failed to connect: " . mysqli_connect_error());
+}
+
+if(isset($_GET['delete'])) {
+    $sno = $_GET['delete'];
+    $delete = true;
+    $sql = "DELETE FROM `notes` WHERE `sno` = $sno";
+    $result = mysqli_query($conn, $sql);
+}
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    if(isset($_POST['snoEdit'])) {
+        //update record
+
+        $sno = $_POST["snoEdit"];
+        $title = $_POST["titleEdit"];
+        $description = $_POST["descriptionEdit"];
+
+        $sql = "UPDATE `notes` SET `title` = '$title' , `description` = '$description'  WHERE `notes`.`sno` = $sno;";
+        $result = mysqli_query($conn, $sql);
+        if($result) {
+            $update = true;
+        } else {
+            echo "Record not updated";
+        }
+
+    } else {
+
+        $title = $_POST["title"];
+        $description = $_POST["description"];
+
+        $sql = "INSERT INTO `notes` (`title`, `description`) VALUES ('$title', '$description')";
+        $result = mysqli_query($conn, $sql);
+
+        if ($result) {
+            $insert = true;
+        } else {
+            $error = true;
+        }
+    }
+}
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+          crossorigin="anonymous">
+          <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="//cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
+
+
+    <title>iNotes - Note taking made easy</title>
+
+</head>
+<body>
+
+<!-- Edit Modal -->
+<div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="editModalLabel">Edit this note</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <form action="/crud/index.php" method="post">
+      <div class="modal-body">
+
+          <input type="hidden" name="snoEdit" id="snoEdit">
+            <div class="mb-3">
+                <label for="title" class="form-label">Note Title</label>
+                <input type="text" class="form-control" id="titleEdit" name="titleEdit" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+                <label for="description" class="form-label">Note Description</label>
+                <textarea class="form-control" id="descriptionEdit" name="descriptionEdit" rows="3"></textarea>
+            </div>
+
+      </div>
+      <div class="modal-footer d-block mr-auto">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary">Save changes</button>
+      </div>
+      </form>
+    </div>
+  </div>
+</div>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#"><img src="/crud/logo.png" height="30px"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" aria-disabled="true"></a>
+                </li>
+            </ul>
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
+    </div>
+</nav>
+
+<?php
+if ($insert) {
+    echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>
+        <strong>Your note has been inserted!</strong> You should check in on some of those fields below.
+        <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+    </div>";
+} elseif ($error) {
+    echo "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
+        <strong>Error:</strong> Unable to insert the note.
+        <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+    </div>";
+}
+?>
+
+<?php
+if ($delete) {
+    echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>
+        <strong>Your note has been deleted!</strong> You should check in on some of those fields below.
+        <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+    </div>";
+} elseif ($error) {
+    echo "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
+        <strong>Error:</strong> Unable to insert the note.
+        <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+    </div>";
+}
+?>
+
+<?php
+if ($update) {
+    echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>
+        <strong>Your note has been updated!</strong> You should check in on some of those fields below.
+        <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+    </div>";
+} elseif ($error) {
+    echo "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
+        <strong>Error:</strong> Unable to insert the note.
+        <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+    </div>";
+}
+?>
+
+<div class="container my-4">
+    <h2>Add a Note to iNotes</h2>
+    <form action="/crud/index.php" method="post">
+        <div class="mb-3">
+            <label for="title" class="form-label">Note Title</label>
+            <input type="text" class="form-control" id="title" name="title" aria-describedby="emailHelp">
+        </div>
+        <div class="mb-3">
+            <label for="description" class="form-label">Note Description</label>
+            <textarea class="form-control" id="description" name="description" rows="3"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Add Note</button>
+    </form>
+</div>
+
+<div class="container my-4">
+    <table class="table" id="myTable">
+        <thead>
+        <tr>
+            <th scope="col">S.No</th>
+            <th scope="col">Title</th>
+            <th scope="col">Description</th>
+            <th scope="col">Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+        $sql = "SELECT * FROM `notes`";
+$result = mysqli_query($conn, $sql);
+$sno = 0;
+while ($row = mysqli_fetch_assoc($result)) {
+    $sno = $sno + 1;
+    echo "     <tr>
+              <th scope='row'>" . $sno . "</th>
+              <td>" . $row['title'] . "</td>
+              <td>" . $row['description'] . "</td>
+              <td> <button class=' edit btn btn-sm btn-primary' id=".$row['sno'].">Edit</button> <button class=' delete btn btn-sm btn-primary' id=d".$row['sno'].">Delete</button </td>
+            </tr>";
+
+}
+
+
+?>
+      </tbody>
+    </table>
+</div>
+<hr />
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+        crossorigin="anonymous"></script>
+
+            <script src="//cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+        <script>
+          let table = new DataTable('#myTable');
+        </script>
+
+        <script>
+        edits=  document.getElementsByClassName('edit');
+          Array.from(edits).forEach((element)=>{
+            element.addEventListener("click", (e)=>{
+              console.log("edit", );
+              tr= e.target.parentNode.parentNode;
+              title=tr.getElementsByTagName("td")[0].innerText;
+              description=tr.getElementsByTagName("td")[1].innerText;
+
+              console.log(title, description);
+              titleEdit.value=title;
+              descriptionEdit.value=description;
+              snoEdit.value=e.target.id;
+              console.log(e.target.id);
+              $('#editModal').modal('toggle');
+
+            })
+          })
+
+          deletes = document.getElementsByClassName('delete');
+    Array.from(deletes).forEach((element) => {
+        element.addEventListener("click", (e) => {
+            console.log("delete", );
+            sno = e.target.id.substr(1); // Remove the comma after substr
+
+            if (confirm("Are you sure you want to delete this note?")) {
+                console.log("yes");
+                // Redirect to delete endpoint
+                window.location = `/crud/index.php?delete=${sno}`;
+
+                //TODO: Use post req to sub form
+
+        }
+        else {
+            console.log("no")
+        }
+              })
+  })
+
+        </script>
+
+</body>
+</html>
